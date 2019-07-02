5a2b0bc68c904e7ba596736e97b005a243c78052
Quynh Nguyen <mr.quynh@gmail.com>
2019-07-03 02:55:59 +0700

    (improv): update version settings

--- Utility.cs
+++ Utility.cs
@@ -22,7 +22,9 @@ using System.Runtime.InteropServices;
 using Microsoft.Extensions.Configuration;
 #endregion
 
+#if !SIGN
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("VIEApps.Components.XUnitTests")]
+#endif
 
 namespace net.vieapps.Components.Utility
 {
@@ -509,12 +511,12 @@ namespace net.vieapps.Components.Utility
 		/// <summary>
 		/// Gets an user-agent as mobile browser
 		/// </summary>
-		public static string MobileUserAgent => "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A356 Safari/604.1";
+		public static string MobileUserAgent => "Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Mobile/15E148 Safari/604.1";
 
 		/// <summary>
 		/// Gets an user-agent as desktop browser
 		/// </summary>
-		public static string DesktopUserAgent => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36";
+		public static string DesktopUserAgent => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15";
 
 		/// <summary>
 		/// Gets the Basic network credential to perform a web request
@@ -525,7 +527,7 @@ namespace net.vieapps.Components.Utility
 		/// <param name="useSecureProtocol"></param>
 		/// <param name="secureProtocol"></param>
 		/// <returns></returns>
-		public static CredentialCache GetWebCredential(Uri uri, string username, string password, bool useSecureProtocol = true, SecurityProtocolType secureProtocol = SecurityProtocolType.Tls12)
+		public static ICredentials GetWebCredential(Uri uri, string username, string password, bool useSecureProtocol = true, SecurityProtocolType secureProtocol = SecurityProtocolType.Tls12)
 		{
 			// check
 			if (uri == null || string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
